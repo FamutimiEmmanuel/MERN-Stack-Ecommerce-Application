f3a7f9d01d4af7eef27326b977fb4b3d59d125ed
FamutimiEmmanuel <olamide.famutimi@gmail.com>
2022-11-14 11:53:35 -0800

    localhost

--- frontend/src/components/Meta.js
+++ frontend/src/components/Meta.js
@@ -12,7 +12,7 @@ const Meta = ({ title, description, keywords }) => {
 }
 
 Meta.defaultProps = {
-  title: 'Welcome To ProShop',
+  title: 'Welcome To Gadgets Place',
   description: 'We sell the best products for cheap',
   keywords: 'electronics, buy electronics, cheap electroincs',
 }
