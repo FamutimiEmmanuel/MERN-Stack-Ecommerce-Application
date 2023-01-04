4b130c5f70c1d9344ee5a845e15dc319dfc98e7d
FamutimiEmmanuel <olamide.famutimi@gmail.com>
2023-01-04 14:10:51 -0800

    build

---
+++ frontend/build/precache-manifest.f2088c2cde16fddbe2c5d631fb29f587.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1ba8cd6be049fc64edcae9ed894a6af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61896aa1767274efc265",
+    "url": "/static/css/main.2988cb80.chunk.css"
+  },
+  {
+    "revision": "478603ddaccdb6f55609",
+    "url": "/static/js/2.8d545eff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8d545eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61896aa1767274efc265",
+    "url": "/static/js/main.82fac117.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
